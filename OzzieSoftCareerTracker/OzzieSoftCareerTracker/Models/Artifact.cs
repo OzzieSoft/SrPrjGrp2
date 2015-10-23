2259483d56867c85585f83b815d5bc11b201e1b0
ProgrammerKyle <kylechaput15@outlook.com>
2015-10-23 15:12:26 -0400

    Fixed a mistake

--- OzzieSoftCareerTracker/OzzieSoftCareerTracker/Models/Artifact.cs
+++ OzzieSoftCareerTracker/OzzieSoftCareerTracker/Models/Artifact.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace OzzieSoftCareerTracker.Models
 {
     public class Artifact
     {
+		public int ArtifactID { get; set; }
+		public string ArtifactName { get; set; }
+		public string ArtifactDescription { get; set; }
     }
+
+	public class ArtifactDBContext : DbContext{
+		public DbSet<Artifact> Artifacts { get; set; }
+	}
 }
