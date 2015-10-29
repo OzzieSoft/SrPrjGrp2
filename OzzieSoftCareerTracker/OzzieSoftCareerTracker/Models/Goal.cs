8b2fb4240d5e8e380a7cd47953d9db202bb412ac
Armando <armandocordero13@gmail.com>
2015-10-29 15:17:07 -0400

    created goal class

---
+++ OzzieSoftCareerTracker/OzzieSoftCareerTracker/Models/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OzzieSoftCareerTracker.Models
+{
+    public class Goal
+    {
+        public int goalID { get; set; }
+        public string goalTitle { get; set; }
+        public string goalDesc { get; set; }
+        public DateTime goalDueDate { get; set; }
+
+        public virtual ICollection<Skill> skills { get; set; }
+        public virtual UserProfile user {get; set; }
+    }
+}
