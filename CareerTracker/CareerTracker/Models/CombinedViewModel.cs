b5e91c1bfe24311dc2871ee511de8ac6de261ad6
unknown <n00809346@CISL-3130-15.unfcsd.unf.edu>
2015-12-03 15:23:25 -0500

    Fixed look, added user profile

---
+++ CareerTracker/CareerTracker/Models/CombinedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CareerTracker.Models
+{
+    public class CombinedViewModel
+    {
+        public Artifact ArtifactModel { get; set; }
+        public Skill SkillModel { get; set; }
+        public Goal GoalModel { get; set; }
+    }
+}
