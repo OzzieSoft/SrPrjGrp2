22ecc369ef6a84757ff77c04707b106794345556
unknown <n00676964@CISVM-SrPro2.unfcsd.unf.edu>
2016-02-04 16:09:53 -0500

    Added file uploading and fixed the details page for artifacts

--- CareerTracker/CareerTracker/Controllers/ArtifactController.cs
+++ CareerTracker/CareerTracker/Controllers/ArtifactController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using CareerTracker.Models;
 using CareerTracker.DAL;
 using CareerTracker.DataRepository;
+using System.IO;
 
 namespace CareerTracker.Controllers
 {
@@ -29,7 +30,12 @@ namespace CareerTracker.Controllers
 
         public ActionResult Details(int id = 0)
         {
-            Artifact artifact = null;//ArtifactRepo.getArtifact(id, User.Identity.Name.ToString());
+            Artifact artifact = db.Artifacts.FirstOrDefault(a => a.ID == id);
+            string username = User.Identity.Name.ToString();
+            if (artifact.User.UserName != username)
+            {
+                artifact = null;
+            }
             if (artifact == null)
             {
                 return HttpNotFound();
@@ -50,14 +56,29 @@ namespace CareerTracker.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Artifact artifact)
+        public ActionResult Create(Artifact artifact, HttpPostedFileBase file)
         {
-            if (ModelState.IsValid)
+             if (ModelState.IsValid)
             {
-                ArtifactRepo.createArtifact(artifact, User.Identity.Name.ToString());
-                return RedirectToAction("Index");
+                 if (file != null && file.ContentLength > 0) {
+                    try
+                    {
+                        string usr = User.Identity.Name.ToString();
+                        string test = Path.GetFileNameWithoutExtension(file.FileName);
+                        var fileName = test + usr + System.IO.Path.GetExtension(file.FileName);
+                        string path = Path.Combine(Server.MapPath("/Artifacts"), fileName);
+                        file.SaveAs(path);
+                        artifact.Location = fileName;
+                        ArtifactRepo.createArtifact(artifact, User.Identity.Name.ToString());
+                        return RedirectToAction("Index");
+                        //ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                    }
+                }
             }
-
             return View(artifact);
         }
 
